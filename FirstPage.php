1a137251da852797ee9dff7865e4272d555ad7e5
vhinle <cscoo2006@gmail.com>
2021-07-29 15:32:51 +0800

    Initial Commit

---
+++ FirstPage.php
@@ -0,0 +1,17 @@
+
+<html>
+    <head>
+        <title>First Page <?php print $title ?></title>
+    </head>
+    <body>
+        <h1>My First PHP Document</h1>
+        <?php
+            
+            $fruites=array("Banana", "Grapes", "Apple");
+
+            echo $fruites[5];
+
+        ?>
+
+    </body>
+</html>
---
+++ process.php
@@ -0,0 +1,14 @@
+
+<html>
+    <head>
+        <title>First Page</title>
+    </head>
+    <body>
+        <?php
+            //echo $_REQUEST['txtdata'];
+            echo $_REQUEST['var'];
+        ?>
+        
+
+    </body>
+</html>
